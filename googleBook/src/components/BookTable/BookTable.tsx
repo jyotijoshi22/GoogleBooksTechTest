d4a2b803430f3c3d43dcf87f61757fd75e05a200
unknown <dhyanijyoti22@gmail.com>
2023-05-03 10:59:36 +1000

    Added search bar

--- googleBook/src/components/BookTable/BookTable.tsx
+++ googleBook/src/components/BookTable/BookTable.tsx
@@ -17,32 +17,47 @@ type BookTableProps = {
 
 const BookTable: React.FC<BookTableProps> = ({ books }) => {
   const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const handleRowClick = (book: Book) => {
     setSelectedBook(book);
   };
 
+  const sortedBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div className={styles["book-table"]}>
-      <table>
-        <thead>
-          <tr>
-            <th>TITLE</th>
-            <th>AUTHORS</th>
-            <th>PUBLISHED DATE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map((book) => (
-            <tr key={book.title} onClick={() => handleRowClick(book)}>
-              <td>{book.title}</td>
-              <td>{book.authors ? book.authors.join(", ") : ""}</td>
-              <td>{book.publishedDate}</td>
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by book title"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+      <div className={styles["book-table"]}>
+        <table>
+          <thead>
+            <tr>
+              <th>TITLE</th>
+              <th>AUTHORS</th>
+              <th>PUBLISHED DATE</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      {selectedBook && <BookDetails book={selectedBook} />}
+          </thead>
+          <tbody>
+            {sortedBooks.map((book) => (
+              <tr key={book.title} onClick={() => handleRowClick(book)}>
+                <td>{book.title}</td>
+                <td>{book.authors ? book.authors.join(", ") : ""}</td>
+                <td>{book.publishedDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {selectedBook && <BookDetails book={selectedBook} />}
+      </div>
     </div>
   );
 };
